4659bea14d9d5dc2ccf60522406862084585ad8c
Kyle Cunningham <klcunningham6@gmail.com>
2014-11-15 20:55:35 +0000

    Updated main.py with some code.

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 #CLIFS/main.py
 #A part of the command line flight simulator.
 #Copyright (c) 2014 ks-salutions.
+import handler
+
+print "Welcome to C L I F S\n"
+print "A command line flight simulator\n"
+print "Copyright (C) Ks-Salutions\n"
+invalue = ""
+while invalue != "q":
+	print "Enter input: M for manual flight, A for automatic flight, or Q to quit.\n"
+	invalue = raw_input()
+	if invalue=="q":
+		break
